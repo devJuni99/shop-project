9f8db8cbcf9c790d5f8d3bcb9b69b7cd6509b232
juni <tmdwns0927@naver.com>
2025-04-02 19:47:37 +0900

    ✨ 관리자, 방문자 네비게이션 처리
    isAdmin props가 있는지 없는지 확인 후, navigation을 동적으로 처리하는 작업

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
     }, []);
 
     console.log("로그인한 유저:", user);
-
+    // 로그인이 안되었다면? login 버튼과 products만 보여주게 설정
     return (
         <header className="flex justify-between border-b border-gray-300 p-2">
             <Link to="/" className="flex items-center text-4xl text-brand">
@@ -21,11 +21,20 @@ export default function Navbar() {
                 <h1>Shoppy</h1>
             </Link>
             <nav className="flex items-center gap-4 font-semibold">
-                <Link to="/products">Products</Link>
-                <Link to="/carts">Carts</Link>
-                <Link to="/products/new" className="text-2xl">
-                    <BsFillPencilFill />
-                </Link>
+                {user && user.isAdmin ? (
+                    <>
+                        <Link to="/products">Products</Link>
+                        <Link to="/carts">Carts</Link>
+                        <Link to="/products/new" className="text-2xl">
+                            <BsFillPencilFill />
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/products">Products</Link>
+                    </>
+                )}
+
                 {user && <User user={user} />}
                 {!user && <button onClick={login}>Login</button>}
                 {user && <button onClick={logout}>Logout</button>}
